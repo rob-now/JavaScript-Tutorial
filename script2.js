54a6fd85c39f01efc1a625b2a02aa41baf7b3fe0
rob-now <rnowakowski.prv@gmail.com>
2018-02-22 12:29:37 +0100

    Updated script2.js

--- script2.js
+++ script2.js
@@ -3,8 +3,28 @@ var myCar = {
     wheels: 4,
     drive: function (speed, time) {
         console.log(speed * time);
+    },
+    test: function(){
+        console.log(this);
+    }
+};
+
+var myCar2 = {
+    maxSpeed: 240,
+    wheels: 6,
+    drive: function (speed, time) {
+        console.log(speed * time);
+    },
+    test: function(){
+        console.log(this);
+    },
+    logWheels: function (){
+        console.log("Number of wheels: " + this.wheels);
     }
 };
 
 console.log(myCar.maxSpeed);
 myCar.drive(120, 3);
+myCar.test();
+myCar2.test();
+myCar2.logWheels();
