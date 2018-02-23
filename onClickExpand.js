2372180fde23f8d7b81fecb8402807fbad403368
rob-now <rnowakowski.prv@gmail.com>
2018-02-23 11:50:34 +0100

    Added event window onload for script placed in the head of HTML

--- onClickExpand.js
+++ onClickExpand.js
@@ -1,18 +1,24 @@
-var content = document.getElementById("content");
-var showMoreBtn = document.getElementById("show-more");
+function setUpEvents() {
+    var content = document.getElementById("content");
+    var showMoreBtn = document.getElementById("show-more");
 
-showMoreBtn.onclick = function(){
-    if (content.className === "open") {
-        // Shrink the box
-        // Erase the class name of open
-        content.className = "";
-        // Change inner HTML
-        showMoreBtn.innerHTML = "Show more";
-    } else {
-        // Expand the box
-        // Give class "open" to the "#content"
-        content.className = "open";
-        // Change inner HTML
-        showMoreBtn.innerHTML = "Show less";
-    }
+    showMoreBtn.onclick = function () {
+        if (content.className === "open") {
+            // Shrink the box
+            // Erase the class name of open
+            content.className = "";
+            // Change inner HTML
+            showMoreBtn.innerHTML = "Show more";
+        } else {
+            // Expand the box
+            // Give class "open" to the "#content"
+            content.className = "open";
+            // Change inner HTML
+            showMoreBtn.innerHTML = "Show less";
+        }
+    };
+}
+
+window.onload = function () {
+    setUpEvents();
 };
